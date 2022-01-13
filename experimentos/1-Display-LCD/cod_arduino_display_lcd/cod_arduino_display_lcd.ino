0194d7eef3ea428c938a22ba5ce9378d3b18b1d6
bingulo <gcarlos@disroot.org>
2022-01-12 21:35:01 -0300

    adequada identação cod exp 1

--- experimentos/1-Display-LCD/cod_arduino_display_lcd/cod_arduino_display_lcd.ino
+++ experimentos/1-Display-LCD/cod_arduino_display_lcd/cod_arduino_display_lcd.ino
@@ -1,23 +1,23 @@
 /*
- *	Material de apoio para as disciplinas:
- *		5910230 - Introdução à Instrumentação Biomédica
- *		5910130 - Eletrônica
+ * Material de apoio para as disciplinas:
+ *	5910230 - Introdução à Instrumentação Biomédica
+ *	5910130 - Eletrônica
  *
- *	Títuo:
- *		Utilizando Display LCD
+ * Títuo:
+ *	Utilizando Display LCD
  *
- *	Autores:
- *		Carlos Eduardo Gallo Filho
- *		Edgar Mazón Valadez
- *		Fábio Eduardo Flores de Lima
- *		Ricardo Ricci Bordonal
- *		Prof. Dr. Theo Zeferino Pavan
+ * Autores:
+ *	Carlos Eduardo Gallo Filho
+ *	Edgar Mazón Valadez
+ *	Fábio Eduardo Flores de Lima
+ *	Ricardo Ricci Bordonal
+ *	Prof. Dr. Theo Zeferino Pavan
  *
- *	Descrição:
- *		Exemplifica a utilização do módulo LCD
+ * Descrição:
+ *	Exemplifica a utilização do módulo LCD
  *
- *	Licença:
- *		GNU GPLv3
+ * Licença:
+ *	GNU GPLv3
  */
 
 // Incluir bibliotecas
@@ -39,26 +39,26 @@ const int dt = 100; //ms
 
 void setup()
 {
-	// Inicia o display 16x2
-	lcd.begin(16, 2);
-	lcd.setCursor(0,0);
-	lcd.print("Iniciando...");
+// Inicia o display 16x2
+    lcd.begin(16, 2);
+    lcd.setCursor(0,0);
+    lcd.print("Iniciando...");
 
-	// Delay de inicialização
-	delay(2000);
+    // Delay de inicialização
+    delay(2000);
 
-	// Limpa o display
-	lcd.clear();
+    // Limpa o display
+    lcd.clear();
 }
 
 void loop()
 {
-	// Exibir texto no display
-	lcd.setCursor(0,0); // Inicia texto na posição 0 da linha 0
-	lcd.print("Texto linha 0");
-	lcd.setCursor(0,1); // Inicia texto na posição 0 da linha 1
-	lcd.print("Texto linha 1");
+    // Exibir texto no display
+    lcd.setCursor(0,0); // Inicia texto na posição 0 da linha 0
+    lcd.print("Texto linha 0");
+    lcd.setCursor(0,1); // Inicia texto na posição 0 da linha 1
+    lcd.print("Texto linha 1");
 
-	// Loop delay
-	delay(dt);
+    // Loop delay
+    delay(dt);
 }
